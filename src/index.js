a19f94ce26fc240b9936c902c03e8d67a20c1606
Michal Sarnecki <michal.sarnecki@stepstone.com>
2020-01-28 22:36:50 +0100

    some changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 const electron = require('electron');
 const path = require('path');
 const fs = require('fs');
+const {dialog, globalShortcut: globalShortcut, app} = electron.remote;
+const Store = require('./configStore');
 
-const {dialog, globalShortcut, app} = electron.remote;
 
 let pathToFile = "";
 let textBuffer = "";
 let published = "";
-
+const htmlTemplate = '<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8"> <title>QuickTexter</title></head><body><span id="content"></span></body></html>';
 
 const autoPublishCheckbox = document.getElementById("auto-publish");
 const publishedArea = document.getElementById("published");
@@ -18,6 +19,7 @@ const publishTextButton = document.getElementById("publish-text-button");
 const clearPublishedButton = document.getElementById("clear-published-text-button");
 const selectFileButton = document.getElementById("select-file-button");
 const newFileButton = document.getElementById("new-file-button");
+const currentFileType = document.getElementById('currentFileType');
 
 
 backToEditButton.addEventListener('click', function () {
@@ -34,17 +36,7 @@ publishTextButton.addEventListener('keyup', function (event) {
     }
 });
 
-autoPublishCheckbox.addEventListener('change', function() {
-    console.log("changed");
-    console.log(autoPublishCheckbox.innerText);
-    if(this.checked) {
-       console.log("Autopublish will be set");
-       setAutoPublish();
-   } else {
-       console.log("Autopublish will be removed");
-       disableAutoPublish();
-   }
-});
+autoPublishCheckbox.addEventListener('change', switchAutoPublish);
 
 clearPublishedButton.addEventListener('click', function() {
     clearPublishedText();
@@ -58,20 +50,68 @@ newFileButton.addEventListener('click', function () {
    newFile()
 });
 
-globalShortcut.register(process.platform === 'darwin' ? 'Command+B': 'Ctrl+B', () => {
-    publish();
-});
+function switchAutoPublish() {
+    if(this.checked) {
+        console.log("Autopublish will be set");
+        setAutoPublish();
+    } else {
+        console.log("Autopublish will be removed");
+        disableAutoPublish();
+    }
+}
 
-globalShortcut.register(process.platform === 'darwin' ? 'Command+P': 'Ctrl+P', () => {
-    publishedBackToBuffor();
-});
+function setUpConfig(callback){
+    callback(new Store({
+        configName: 'user-preferences',
+        defaults: {
+            'globalShortKeys': {
+                'publish': {
+                    'mac': 'Command+P',
+                    'win': 'Ctrl+P',
+                },
+                'backToBuffor': {
+                    'mac': 'Command+D',
+                    'win': 'Ctrl+D',
+                },
+                'clearPublished': {
+                    'mac': 'Command+D',
+                    'win': 'Ctrl+D',
+                },
+                'switchAutoPublish': {
+                    'mac': 'Command+L',
+                    'win': 'Ctrl+L',
+                }
+            }
+        }
+    }))
+}
 
-globalShortcut.register(process.platform === 'darwin' ? 'Command+E': 'Ctrl+E', () => {
-    clearPublishedText();
-});
+setUpConfig(registerGlobalShortKeys);
 
+function registerGlobalShortKeys(configStore)
+{
+    console.log(configStore);
+    let shortKeys = configStore.get('globalShortKeys');
+    console.log(shortKeys);
+    globalShortcut.register(process.platform === 'darwin' ? shortKeys['publish']['mac']: shortKeys['publish']['win'], () => {
+        publish();
+    });
+
+    globalShortcut.register(process.platform === 'darwin' ?  shortKeys['backToBuffor']['mac']: shortKeys['backToBuffor']['win'], () => {
+        publishedBackToBuffor();
+    });
+
+    globalShortcut.register(process.platform === 'darwin' ? shortKeys['clearPublished']['mac']: shortKeys['clearPublished']['win'], () => {
+        clearPublishedText();
+    });
+
+    globalShortcut.register(process.platform === 'darwin' ? shortKeys['switchAutoPublish']['mac']: shortKeys['switchAutoPublish']['win'], () => {
+        switchAutoPublish();
+    });
+}
 
 function setAutoPublish() {
+    console.log(configStore.get('test'));
     console.log("set auto-publish");
     bufferedArea.addEventListener("input", publish)
 }
@@ -82,11 +122,11 @@ function disableAutoPublish() {
 }
 
 function clearPublishedText() {
-    if( !path ) {
+    if( !pathToFile ) {
         alert("Select file first");
         return false;
     }
-
+    configStore.set('test2', '1');
     publishedArea.value = "";
     published = "";
     saveToFile(pathToFile, "");
@@ -136,35 +176,50 @@ function selectFilePath() {
             ]
         }
         ).then(result => {
-        console.log(result.filePaths);
-        if (undefined == result.filePaths[0]) {
-            console.log('Button clicked, but no file created');
-            return;
-        }
+            console.log(result.filePaths);
+            if (undefined == result.filePaths[0]) {
+                console.log('Button clicked, but no file created');
+                return;
+            }
 
-        if ("txt" === result.filePaths[0].split('.').pop()){
-            document.getElementById('currentFileType').innerText = 'txt';
-        } else if ("html" === result.filePaths[0].split('.').pop()){
-            document.getElementById('currentFileType').innerText = 'html';
-        } else {
-            console.log("Error occured: " + err.message);
-            return;
-        }
-        pathToFile = result.filePaths[0];
-        filePathValue.innerText = result.filePaths[0];
+            let filePath =  result.filePaths[0];
+            pathToFile = result.filePaths[0]
 
+            if ("txt" === filePath.split('.').pop()){
+                fs.readFile(filePath, "utf8", function (err, data) {
+                    if (err) {
+                        throw err;
+                    }
+                    console.log(data);
+                    bufferedArea.value = data;
+                });
+                currentFileType.innerText = 'txt';
 
-        readFileContent(pathToFile, function (content) {
-            bufferedArea.innerText = content;
-        });
+            } else if ("html" === filePath.split('.').pop()){
+                fs.readFile(filePath, "utf8", function (err, data) {
+                    if (err) {
+                        throw err;
+                    }
+                    console.log(data);
+                    bufferedArea.value = data.match('<body>(.*)</body>')[1];
+                });
+                currentFileType.innerText = 'html';
+            } else {
+                console.log("Error occured: " + err.message);
+                return;
+            }
+
+            filePathValue.innerText = filePath;
     })
 }
 
 function publish(){
     console.log("Publish");
     textBuffer = bufferedArea.value;
+    console.log("Publish path = " + path);
 
-    if( !path ) {
+    if( !pathToFile) {
+        console.log("Publish path = " + path);
         alert("Select file first");
         return false;
     }
@@ -176,18 +231,32 @@ function publish(){
 
 function saveToFile(path, content) {
 
-    fs.writeFile(path, content, (err) => {
-        if (err) {
-            console.log("Error occured: " + err.message);
-        }
-    });
+    if("txt" === currentFileType.innerText) {
+        fs.writeFile(path, content, (err) => {
+            if (err) {
+                console.log("Error occured: " + err.message);
+            }
+        });
+    } else if ("html" === currentFileType.innerText)  {
+        content = includeContentInHtml(content);
+        fs.writeFile(path, content, (err) => {
+            if (err) {
+                console.log("Error occured: " + err.message);
+            }
+        });
+    } else {
+        console.log("ERROR: not writeable format of the file");
+    }
+}
+
+function includeContentInHtml(content) {
+    return htmlTemplate.replace('<span id="content"></span>', content);
 }
 
 function newFile() {
 
     let content;
     let contentTxt = "";
-    let contentHtml = '<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8"> <title>QuickTexter</title> <link rel="stylesheet" href="../main.css"></head><body></body></html>';
 
     const options = {
         defaultPath: app.getPath('documents') + '/',
@@ -206,7 +275,7 @@ function newFile() {
                 content = contentTxt;
             } else if ("html" === result.filePath.split('.').pop()){
                 document.getElementById('currentFileType').innerText = 'html';
-                content = contentHtml;
+                content = htmlTemplate;
             } else {
                 console.log("Error occured: " + err.message);
                 return;
@@ -222,8 +291,6 @@ function newFile() {
 
                 alert("File created successfuly: " + result.filePath);
             });
-
-
         }
     );
 }
